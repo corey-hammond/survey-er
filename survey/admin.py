5b25a98082426de363a26719032dd266763cc8cb
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-11-07 11:48:12 -0800

    created pages:index and _navbar partial; configured navbar links; configured admin page

--- survey/admin.py
+++ survey/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
+admin.site.site_header = 'Survey-er Admin'
+admin.site.site_title = 'Survey-er Admin Area'
+admin.site.index_title = 'Welcome to the Survey-er Admin Area'
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- survey_er/urls.py
+++ survey_er/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('survey/', include('survey.urls')),
     path('admin/', admin.site.urls),
 ]
